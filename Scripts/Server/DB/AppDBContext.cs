75421a3436ce79654f35a0ea01d1d9c7062231ff
Ahwhi <jwen7@naver.com>
2025-12-12 15:39:13 +0900

    Add files via upload

---
+++ Scripts/Server/DB/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static Server.DB.DataModel;
+
+namespace Server.DB
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            var baseDir = AppContext.BaseDirectory;
+            var dbPath = Path.Combine(baseDir, "Data", "GameDB.db");
+            Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Account>().ToTable("Accounts").HasIndex(u => u.accountId).IsUnique();
+        }
+    }
+}
---
+++ Scripts/Server/DB/DataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DB
+{
+    public class DataModel
+    {
+        public class Account {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            public string accountId { get; set; }
+            public string passwordHash { get; set; }
+            public string recentIp { get; set; }
+            public string? accessToken { get; set; }
+            public DateTime? tokenExpireAt { get; set; }
+            public int winGame {  get; set; }
+            public int loseGame { get; set; }
+            public float winPercentage { get; set; }
+        }
+    }
+}
